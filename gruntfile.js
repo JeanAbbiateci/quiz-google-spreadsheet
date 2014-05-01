ffa61fd43169acd5a3c26dc015204892612aed16
JeanAbbiateci <j.abbiateci@gmail.com>
2014-05-01 15:55:18 +0200

    add

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,40 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		connect: {
-			
-				port: 8000,
-				
-			
-		},
-		
-		watch: {
-      options: {
-        livereload: true,
-      },
-      html: {
-        files: ['index.html'],
-      },
-      js: {
-        files: ['js/**/*.js'],
-        
-      },
-      css: {
-        files: ['css/scss/**/*.scss'],
-        
-      },
-      
-      css: {
-        files: ['css/scss/**/*.scss'],
-        
-      },
-		
-		
-		
-	});
- 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-connect');
-	grunt.registerTask('default', 'connect','watch');
-	
-};
+    grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+            connect: {
+
+                port: 8000,
+                base: "localhost"
+
+
+            },
+
+            watch: {
+                options: {
+                    livereload: true,
+                },
+                html: {
+                    files: ['index.html'],
+                },
+                js: {
+                    files: ['js/**/*.js'],
+
+                },
+                css: {
+                    files: ['css/scss/**/*.scss'],
+
+                },
+
+                css: {
+                    files: ['css/scss/**/*.scss']
+
+                },
+
+}
+
+            }); 
+    		grunt.loadNpmTasks('grunt-contrib-watch');
+            grunt.loadNpmTasks('grunt-connect'); 
+            grunt.registerTask('dev' ['watch','connect']);
+    };
