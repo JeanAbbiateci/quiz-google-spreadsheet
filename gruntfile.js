6eb4819020c9ffba30772a6e2221a6ead363069c
JeanAbbiateci <j.abbiateci@gmail.com>
2014-05-01 15:35:04 +0200

    Update gruntfile.js
    add file

--- gruntfile.js
+++ gruntfile.js
@@ -7,10 +7,34 @@ module.exports = function(grunt) {
 				port: 8000,
 				
 			
-		}
+		},
+		
+		watch: {
+      options: {
+        livereload: true,
+      },
+      html: {
+        files: ['index.html'],
+      },
+      js: {
+        files: ['js/**/*.js'],
+        
+      },
+      css: {
+        files: ['css/scss/**/*.scss'],
+        
+      },
+      
+      css: {
+        files: ['css/scss/**/*.scss'],
+        
+      },
+		
+		
+		
 	});
-
+ 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-connect');
-	grunt.registerTask('default', 'connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', 'connect','watch');
+	
 };
